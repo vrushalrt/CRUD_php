131b19cd1fbe355a8da45e856bdc417aaa34a003
vrushalrt <vrushalrt@gmail.com>
2016-12-04 00:59:02 +0530

    second commit

---
+++ application/views/main.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> CRUD </title>
+</head>
+<body>
+<h1><tt>HELLO CRUD</tt></h1><br>
+
+<form method="POST" action=" ">
+<input type="submit">
+</form>
+	
+
+</form>
+</body>
+</html>
